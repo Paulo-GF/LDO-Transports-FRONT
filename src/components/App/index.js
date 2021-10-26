683a3eea6f83defcf6df874f5c24ae65326d38ea
HeleneLoriot <loriot.helene@gmail.com>
2021-10-26 14:52:09 +0200

    initialization of the global architecture

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,10 +5,9 @@ import './styles.css';
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
   </div>
 );
 
 // == Export
 export default App;
+
