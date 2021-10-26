ca741953343f7bf09181562f8acafd8e90c9d5f4
HeleneLoriot <loriot.helene@gmail.com>
2021-10-26 17:59:56 +0200

    create static component History in Home

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,15 @@
+// import components
+import Home from 'src/components/Home';
+
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import './styles.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
+    <Home />
   </div>
 );
 
 // == Export
 export default App;
-
---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+// import components
+import History from 'src/components/Home/History';
+
+// import styles
+import './styles.scss';
+
+export default function Home() {
+  return (
+    <History />
+  );
+}
