b98760e0c0cc196535a75820530f8bdd4817a97c
RemiHENRY0117 <remi2.henry@gmail.com>
2021-10-27 18:03:32 +0200

    router ready, has to be updated when the backend is ready to send data

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,40 +1,74 @@
-// import components
+// Import
+// import axios from 'axios';
+import { useState } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+// Import components
 import Home from 'src/components/Home';
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
 
-// == Import
-// import { Switch, Route } from 'react-router-dom';
+// Import style
 import './styles.scss';
 
-// == Composant
-const App = () => (
-  <div className="app">
-    <Header />
-    <Home />
-    <Footer />
-  </div>
-);
+// Composant
+export default function App() {
+  // fake data while we wait for the backend
+  const [isLogged, setIsLogged] = useState(true);
+  const adminInfo = 'Michel';
+  const logOut = () => {
+    console.log('admin logged out');
+  };
+  /*
+  skeleton to implement/complete when the backend is ready
+  const adminInfo = () => {
+    axios.get('')
+      .then((response) => {
+        setIsLogged(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+*/
 
-// == Export
-export default App;
+  return (
+    <div className="app">
+      <Header
+        adminInfo={adminInfo}
+        isLogged={isLogged}
+        logOut={logOut}
+      />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+      <Footer />
+    </div>
+  );
+}
 
 /*
-To add later on, freshly baked
-<Switch>
-  <Route exact path="/">
-    <Home />
+To add later on when all pages are ready
+
+<Route exact path="/recrutement">
+  <Recrutement />
+</Route>
+<Route exact path="/contact">
+  <Contact />
+</Route>
+
+ternary expression to only allow access to the admin route if admin logged
+{isLogged && (
+  <Route exact path="/admin-logged">
+    <AdminPage />
   </Route>
-  <Route exact path="/recrutement">
-    <Recrutement />
-  </Route>
-  <Route exact path="/contact">
-    <Contact />
-  </Route>
-  {isLogged && (
-    <Route exact path="/admin-logged">
-      <AdminPage />
-    </Route>
-  )}
-</Switch>
+)}
+
+<Route>
+  <LegalNotices />
+<Route >
+  <404 />
+</Route>
 */
