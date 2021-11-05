d5d8681b39f9ac98fe170baf8db44d21d2a5641d
RemiHENRY0117 <remi2.henry@gmail.com>
2021-11-05 11:16:33 +0100

    beautifully done

---
+++ src/components/Notfound/index.js
@@ -0,0 +1,12 @@
+// import styles
+import './styles.scss';
+
+export default function Notfound() {
+  return (
+    <div className="Notfound-container">
+      <div className="Notfound-content">
+        <h1 className="Notfound-title"> Sorry bro ! </h1>
+      </div>
+    </div>
+  );
+}
